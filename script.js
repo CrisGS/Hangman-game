718a5a429550e3f13354483ad99a43e7fc84ad4a
CrisGS <93886227+CrisGS@users.noreply.github.com>
2022-05-10 11:33:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-let letter;
+let guessedLetters = 0;
 let lifes = 6;
 const wordToGuess = [];
+const triedLetters = [];
+
+
 
 function addWordLetters() {
-  document.getElementById("lifes").innerHTML = lifes;
   let word = document.getElementById("enteredWord").value;
   let wordLength = word.length;
-  if (wordLength >= 6) {
-    for (let i = 0; i < wordLength; ++i) {
-      let wordLetters = document.createElement("li");
-      wordLetters.id = 'lettersPlace';
-      wordToGuess[i] = word[i];
-      wordLetters.innerHTML = "<li class='hidenLetter' id='"+ letter +"'>  __  </li>";
-      document.body.appendChild(wordLetters);
+  let onlyLetters = /^[A-Za-z]+$/;
+  if (onlyLetters.test(document.getElementById("enteredWord").value)) {
+    if (wordLength >= 4) {
+      document.getElementById("lifes").innerHTML = lifes;
+      let line = "__";
+      for (let i = 1; i < wordLength - 1; ++i) {
+        wordToGuess[i] = line;
+      }
+      wordToGuess[0] = word[0];
+      wordToGuess[wordLength - 1] = word[wordLength - 1];
+      document.getElementById("lettersPlace").innerHTML = wordToGuess.join(' ');
+    } else {
+      alert("Please enter a word consisting of 6 or more letters.");
+      location.reload();
     }
   } else {
-    alert("Please enter a word at least 6 letters long.")
+    alert("The word entered must contain only letters!");
     location.reload();
   }
   document.getElementById("subButton").disabled = true;
 }
+
 
 function pressedLetter(event) {
   let pressedKey = event.key;
@@ -27,13 +37,45 @@ function pressedLetter(event) {
   let input = document.getElementById("enteredWord").value;
   let check = input.includes(pressedKey);
   if (event.keyCode >= 65 && event.keyCode <= 90 && submitBtn == 1) {
-    if (check == true) {
-      alert("Litera se gaseste in cuvant");
+    document.getElementById("allTriedLetters").appendChild(document.createTextNode(pressedKey + " "));
+    if (triedLetters.includes(pressedKey)) {
+      alert("You have already clicked on this letter. Try another one!");
     } else {
-      alert("Litera nu se gaseste in cuvant");
-      document.getElementById("incorectGuess").appendChild(document.createTextNode(pressedKey + " "));
-      document.getElementById("lifes").innerHTML = --lifes;
+      triedLetters.push(pressedKey);
+      if (check == true) {
+        for (i = 1; i < input.length - 1; ++i) {
+          if (pressedKey == input[i]) {
+						++guessedLetters;
+            wordToGuess[i] = pressedKey;
+            document.getElementById("lettersPlace").innerHTML = wordToGuess.join(' ');
+          }
+        }
+      } else if (lifes > 0) {
+        document.getElementById("incorectGuess").appendChild(document.createTextNode(pressedKey + " "));
+        document.getElementById("lifes").innerHTML = --lifes;
+      }
+    }
+    let div = document.getElementById("displayResult");
+    let letterToGuess = input.length - 2;
+    if (lifes == 0) {
+      document.getElementById("playZone").remove();
+      document.getElementById("guessingPlace").remove();
+      document.getElementById("displayResult").innerHTML = "GAME OVER!";
+      div.style.color = "red";
+      let resetBtn = "<button id='resetBtn' onclick='resetGame()'>Play again!</button>";
+      document.getElementById("resetButton").innerHTML = resetBtn;
+    }
+		if (guessedLetters === letterToGuess) {
+      document.getElementById("playZone").remove();
+      document.getElementById("guessingPlace").remove();
+      document.getElementById("displayResult").innerHTML = "GAME WON!";
+      div.style.color = "green";
+      let resetBtn = "<button id='resetBtn' onclick='resetGame()'>Play again!</button>";
+      document.getElementById("resetButton").innerHTML = resetBtn;
     }
   }
 }
 
+function resetGame() {
+  location.reload();
+}
